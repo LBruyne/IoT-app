6a302249600aa00e52ae4e2f05fc85707ceb4ee6
LBruyne <519102931@qq.com>
2021-03-25 14:18:55 +0800

    Model for backend.

---
+++ backend/src/main/java/com/hinsliu/iotapp/domain/model/IoTDeviceDO.java
@@ -0,0 +1,92 @@
+package com.hinsliu.iotapp.domain.model;
+
+import java.io.Serializable;
+
+/**
+ * @Description: model for IoT devices.
+ * @author: liuxuanming
+ * @date: 2021/03/25 1:33 下午
+ */
+public class IoTDeviceDO implements Serializable {
+
+    private Integer id;
+
+    private String name;
+
+    private String description;
+
+    private Integer state;
+
+    private String createTime;
+
+    private Integer userId;
+
+    private Integer type;
+
+    private String updateTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getState() {
+        return state;
+    }
+
+    public void setState(Integer state) {
+        this.state = state;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public String getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(String updateTime) {
+        this.updateTime = updateTime;
+    }
+
+}
---
+++ backend/src/main/java/com/hinsliu/iotapp/web/controller/LoginController.java
@@ -0,0 +1,9 @@
+package com.hinsliu.iotapp.web.controller;
+
+/**
+ * @Description: controller for login and registry services
+ * @author: liuxuanming
+ * @date: 2021/03/25 1:39 下午
+ */
+public class LoginController {
+}
